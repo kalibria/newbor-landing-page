91c9a86740fba599e3bd3a2bc08f82b4da17587f
Maria Kushlianskaya <mariakazakova92@gmail.com>
2021-04-14 22:56:25 +0300

    finish main loan calculator logic

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -17,39 +17,45 @@ btnCalc.onclick = calculate;
 function validateSum (creditInput) {
     if(typeof creditInput === "string") {
         alert("Введите число");
+        return false;
     }else if(creditInput < 0) {
-        alert("Введите корректное значение")
+        alert("Введите корректное значение");
+        return false;
     }else{
-        return;
+        return true;
     }
 }
 
-function validateAge (ageInput) {
+ function validateAge (ageInput) {
     if (ageInput < 18 || ageInput > 55) {
-        alert ("Ваш возраст не позволяет взять кредит");
+        //alert ("Ваш возраст не позволяет взять кредит"); 
+        return false;
     }else {
-        return
+        return true;
     } 
 }
 
 
-function validateSalaryInput (salaryInput) {
+let validateSalaryInput = function (salaryInput) {
     if(typeof salaryInput !== "number"){
         alert ("Введено неправильное значение");
+        return false;
     }
+    return true;
 }
 
 
-function validateSalary (salaryInput,monthlyPayment) {
+let validateSalary = function (salaryInput,monthlyPayment) {
     percentageOfSalary = salaryInput * 0.6;
     if (monthlyPayment > percentageOfSalary) {
         alert("К сожалению, Вы не сможете взять кредит. Ежемесячные платежи составят более 60% от суммы заработной платы")        
+        return false;
     }else {
-        return;
+        return true;
     }
 }
 
-function calculateMonthlyPayment (interestRate, sum, term) {
+let calculateMonthlyPayment = function  (interestRate, sum, term) {
     let monthlyPercent = interestRate / 12;
     let sumMonthlyPercent = sum * monthlyPercent / 100;
     let sumPercent = sumMonthlyPercent * term;
@@ -60,28 +66,30 @@ function calculateMonthlyPayment (interestRate, sum, term) {
 function calculate (ev) {
     ev.preventDefault();
 
-console.log("hohoh");
-
     sum = +formCalculator.sum.value;
     age = +formCalculator.age.value;
     salary = +formCalculator.salary.value;
     term = +formCalculator.term.value;
 
-    validateSum(sum);
-    validateAge(age);
-    validateSalaryInput(salary);
+// todo remove double alerts
+
+    const isValidSum = validateSum(sum);
+    const isValidAge = validateAge(age);
+    const isValidSalary = validateSalaryInput(salary);
     const monthlyPayment = calculateMonthlyPayment(rate, sum, term);
     validateSalary(salary,monthlyPayment);
- 
 
-    if (age >= 18 && age <= 55 && monthlyPayment <= percentageOfSalary) {
-        alert("Ваш ежемесячный платеж составит " + monthlyPayment + " руб."); 
-    }else{
+    if (isValidSum === false ){
         return;
+    }else if (isValidAge === false) {
+        console.log(isValidAge,"age");
+        return;
+    }else if (isValidSalary === false) {
+        return;
+    }else {
+        alert("Ваш ежемесячный платеж составит " + monthlyPayment + " руб.");
     }
 }
+ 
 
-
-
-
-
+   
